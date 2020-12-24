e97700611470d82b7bef7bd18f99e900737a60c9
Vico Melo <vinicomelo@gmail.com>
2020-12-24 01:01:54 -0500

    added account, assets, orders, positions modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AccountModule } from './account/account.module';
+import { OrdersModule } from './orders/orders.module';
+import { PositionsModule } from './positions/positions.module';
+import { AssetsModule } from './assets/assets.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({}), AccountModule],
+  imports: [ConfigModule.forRoot({}), AccountModule, OrdersModule, PositionsModule, AssetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
